246584eb622ffdc6bb8be5fecaf13500155e6603
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-05-24 15:05:49 -0600

    Got Parallax animations working and sliding effect done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import imagesObj from './groups/imagesObj';
 import Stars from "./components/Stars/Stars";
 import Welcome from './components/Welcome';
-import imagesObj from './groups/imagesObj';
+// import DisplayModal from './components/Modal';
+import styled from "styled-components";
+import { Switch } from "@mui/material";
+
+const TestStyledDiv = styled.div`
+  .no-slide{
+    transition: transform 3s;
+    transform: translate(0%);
+  }
+
+  .slide-left{
+    transition: 3s;
+    transform: translate(-45%, 10%);
+  }
+
+  .slide-right{
+    transition: 2s;
+    transform: translate(20%, 20%);
+  }
+
+  .bluey{
+    color: teal;
+  }
+  .redy{
+    color: crimson;
+  }
+`
 
 function App() {
+  // const [modalOpen, setModalOpen] = useState(false);
+  const [treesOpen, setTreesOpen] = useState(false);
   const ref = useRef()
-  const { leftMountain, rightMountain, leftTrees, rightTrees } = imagesObj
+  const { leftMountain, rightMountain, leftTrees, rightTrees } = imagesObj;
+
+  // const toggleModal = () => { setModalOpen(!modalOpen) }
 
   return (
     <div className="App">
@@ -68,30 +99,55 @@ function App() {
 
         </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1}
-          factor={1.0}
-          speed={0.15}
-          style={{
-            backgroundImage: `url(${leftTrees})`,
-            backgroundSize: "100%",
-          }}
-          
-        >
-        </ParallaxLayer>
-      
-        <ParallaxLayer
-          offset={1}
-          factor={1.2}
-          speed={0.25}
-          style={{
-            backgroundImage: `url(${rightTrees})`,
-            backgroundSize: "100%",
-          }}
-          
-        >
-        </ParallaxLayer>
+        <TestStyledDiv>
+          <ParallaxLayer
+            className={!treesOpen ? "no-slide" : "leftTrees slide-left"}
+            offset={1}
+            factor={1.0}
+            speed={0.15}
+            // style={{
+            //   transition: "2s",
+            //   backgroundImage: `url(${leftTrees})`,
+            //   backgroundSize: "100%",
+            //   backgroundPositionX: treesOpen ? "left 500px" : "defualt"
+            // }}
+            
+          >
+            <img src={leftTrees} alt="leftTrees." width={"100%"} className={!treesOpen ? 'no-slide' : "slide-left"} />
+          </ParallaxLayer>
+        
+          <ParallaxLayer
+            offset={1}
+            factor={1.2}
+            speed={0.25}
+            // style={{
+            //   transition: "2s",
+            //   backgroundImage: `url(${rightTrees})`,
+            //   backgroundSize: "100%",
+            //   backgroundPositionX: treesOpen ? "right 500px" : "defualt"
+            // }}
+            
+          >
+            <img src={rightTrees} alt="rightTrees" width={"100%"} className={!treesOpen ? 'no-slide' : "slide-right"} />
+          </ParallaxLayer>
 
+          <ParallaxLayer
+            offset={1.24}
+            factor={0.4}
+            speed={1}
+          >
+            {/* {modalOpen 
+              ? <DisplayModal openModal={openModal} closeModal={closeModal} /> 
+              : <div><h3>See some projects</h3><span onClick={openModal}>Click Here</span></div>
+            } */}
+            <div onClick={() => setTreesOpen(!treesOpen)}>
+                <div className='example'>
+                    <Switch defaultChecked />
+                </div>
+            </div>
+            
+          </ParallaxLayer>
+        </TestStyledDiv>
       
       </Parallax>
     </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,60 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Modal, Box, Button } from "@mui/material";
-import { Close } from "@mui/icons-material";
+import CloseIcon from "@mui/icons-material/Close";
+import playroom from "../assets/acornsSnap.png";
 
 export default function DisplayModal(props){
-    const [category, setCategory] = useState("");
-    const [subCategory, setSubCategory] = useState("");
+    // const [category, setCategory] = useState("");
+    // const [subCategory, setSubCategory] = useState("");
     const {openModal, closeModal} = props;
 
     return (
         <Modal
             open={openModal}
             onClose={closeModal}
+            sx={{
+                padding: "5px 8px",
+                marginTop: "25vh",
+                marginLeft: "35vh",
+            }}
         >
-            <Box>
-                <Button>
-                    <Close />
+            <Box
+                sx={{
+                    width: "70%",
+                    height: "60%",
+                    borderRadius: "8px",
+                    bgcolor: "rgba(196, 196, 196, 0.3)",
+                    boxShadow: 24,
+                    p: 7,
+                }}
+                style={{ position: "relative" }}
+            >
+                <Button
+                    sx={{
+                        position: "absolute",
+                        top: 15,
+                        right: 5,
+                    }}
+                    onClick={closeModal}
+                >
+                    <CloseIcon />
                 </Button>
+                <div className="modaling">
+                    <div className="dropdown">
+                        <h4><u>Preschool</u></h4>
+                        <h4>Resturaunt</h4>
+                        <h4>Merch Store</h4>
+                    </div>
+                    <div className="body">
+                        <img src={playroom} id="acorns" alt={"playroom"}></img>
+                    </div>
+                    <div className="description">
+                        <h5>Visit this site to see:</h5>
+                        <h6>Photo Carosels, form inquiries, home business</h6>
+                    </div>
+                </div>
+                
+                
 
             </Box>
 
