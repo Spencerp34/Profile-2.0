7298ba4f6df9d8b6a4f1b7b5825ffb4a707c567a
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-06-02 18:27:05 -0600

    added scafolding for projects

---
+++ src/groups/options.js
@@ -0,0 +1,16 @@
+const options = [
+    {
+        Short: "Preschool",
+        Name: "Little Acorns",
+    },
+    {
+        Short: "Resturaunt",
+        Name: "Midnight Snax",
+    },
+    {
+        Short: "Merch Store",
+        Name: "Pyramid Planet",
+    },
+]
+
+export default options
