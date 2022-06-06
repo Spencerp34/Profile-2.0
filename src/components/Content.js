0207c3a603380cc9434c754edd839a2f461c6cb1
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-06-06 14:07:10 -0600

    rudimentary navigational arrows working

--- src/components/Content.js
+++ src/components/Content.js
@@ -12,37 +12,18 @@ const ContentDiv = styled.div`
     }
 
     .project-title{
-        bottom: 0%;
+        bottom: -20%;
         right: 40%;
         text-align: center;
         letter-spacing: 10px;
-        margin-top: -60px;
         padding-left: 10px;
         -webkit-background-clip: text;
         text-decoration: underline;
-    }
-    .project-title:hover {
-        text-decoration: underline;
-    }
-    .right{
-        top: 50%;
-        right: 2%;
-    }
-    .left{
-        top: 50%;
-        left: 2%
-    }
-    
+    }    
 `
 
 const Content = (props) => {
     const {treesOpen, setTreesOpen} = props
-    const handleLeft = () => {
-        console.log("going left")
-    }
-    const handleRight = () => {
-        console.log("going right")
-    }
 
     return(
         <ContentDiv>
@@ -51,26 +32,12 @@ const Content = (props) => {
                         <DisplayModal  {...props} />
                     </div>
                 :   
-                    <section className="navigation-arrows">
-                        <div 
-                            className="left primaryWhite"
-                            onClick={() => handleLeft()}
-                        >
-                                ◀
-                        </div>
-                        <div 
-                            className="project-title primaryWhite" 
-                            onClick={() => setTreesOpen(true)}
-                        >
-                            See My Projects ▼
-                        </div>
-                        <div 
-                            className="right primaryWhite"
-                            onClick={() => handleRight()}
-                        >
-                            ▶
-                        </div>
-                    </section>
+                    <div 
+                        className="project-title primaryWhite" 
+                        onClick={() => setTreesOpen(true)}
+                    >
+                        See My Projects ▼
+                    </div>
             }
         </ContentDiv>
     )
