850f75bf6070995ceb423e44730386ad78314716
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-06-06 13:41:14 -0600

    added a more stable class switch case for parallax layers

---
+++ src/components/Cavewall.js
@@ -0,0 +1,16 @@
+import cavewallimg from "../assets/cavewall.jpg"
+import styled from "styled-components";
+
+const CavewallDiv = styled.div`
+    background: url(${cavewallimg})
+`
+
+const Cavewall = () => {
+    return(
+        <div className="Cavewall">
+
+        </div>
+    )
+}
+
+export default Cavewall;
