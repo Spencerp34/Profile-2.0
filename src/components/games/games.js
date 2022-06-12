ec88fd5e8e31e052fb7bae6e4c29c7b916d80762
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-06-12 15:10:27 -0600

    styled Games component more

--- src/components/games/games.js
+++ src/components/games/games.js
@@ -1,9 +1,37 @@
+import styled from "styled-components";
+
+const GamesDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    left: 10%;
+    top: -20%;
+    display:inline-block;
+    width: 80vw;
+    .Games{
+        color: black;
+        background-color: rgba(196, 196, 196, 0.3);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        min-height: 80vh;
+        border-radius: 15px;
+    }
+`
+
 const Games = (props) => {    
 
     return(
-        <div className="Games">
-            Welcome to the Games!
-        </div>
+        <GamesDiv>
+            <div className="Games">
+                <div className="Intro">
+                    Welcome to the Games!
+                </div>
+                
+            </div>            
+        </GamesDiv>
     )
 }
 
