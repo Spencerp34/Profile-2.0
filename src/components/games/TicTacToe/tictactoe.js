f914f9724c8820f4914df76e4adbe9832de1c7d3
Spencer Patterson <spencerpatterson@Spencers-Mac-mini.local>
2022-06-13 18:02:17 -0600

    added all styles and organized in folder

---
+++ src/components/games/TicTacToe/tictactoe.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import {TicTacToeDiv} from "./tttStyle";
+
+
+const TicTacToe = (props) => {
+    const [turn, setTurn] = useState("o")
+
+    return(
+        <TicTacToeDiv>
+            <div className="TicTacToe">
+                <div className={`board ${turn}`}>
+                    <div className="cell x"></div>
+                    <div className="cell o"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                </div>
+                <div className="winningMessage ">
+                    X is the 
+                    <br/>
+                    Winner! 
+                    <button id="resetBtn">
+                        Restart
+                    </button>
+                </div>
+            </div>
+
+        </TicTacToeDiv>
+    )
+}
+
+export default TicTacToe;
